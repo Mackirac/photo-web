057fc6f10f7e35634744fc36f97fa90dccd20b26
mcorrea <mcorrea.comp@gmail.com>
2019-10-16 13:31:23 -0300

    Sepia implemented

--- color.py
+++ color.py
@@ -27,3 +27,23 @@ def weighted_grayscale (image, weights):
                 opixel += ipixel[c] * weights[c]
             output.append(int(opixel))
     return Image.frombytes('L', image.size, bytes(output))
+
+def sepia (image):
+    bands, mode, size, data =\
+        len(image.getbands()), image.mode, image.size, list(image.tobytes())
+    if bands < 3: raise TypeError("Image with less than 3 channels")
+
+    for i in range(0, len(data), bands):
+        pixel = (data[i], data[i+1], data[i+2])
+        data[i] = int(0.393*pixel[0] + 0.769*pixel[1] + 0.189*pixel[2])
+        if data[i] < 0: data[i] = 0
+        if data[i] > 255: data[i] = 255
+
+        data[i+1] = int(0.349*pixel[0] + 0.686*pixel[1] + 0.168*pixel[2])
+        if data[i+1] < 0: data[i+1] = 0
+        if data[i+1] > 255: data[i+1] = 255
+
+        data[i+2] = int(0.272*pixel[0] + 0.534*pixel[1] + 0.131*pixel[2])
+        if data[i+2] < 0: data[i+2] = 0
+        if data[i+2] > 255: data[i+2] = 255
+    return Image.frombytes(mode, size, bytes(data))
